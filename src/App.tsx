5caa29e89499abd1e5273c4e31697600402ecb0c
bachidze <bachidze90@gmail.com>
2024-02-03 01:38:17 +0400

    Feat(clg)remove slg

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,6 @@ function App() {
         setTime3(res.data.timezone);
         setDayOfTheY(res.data.day_of_year)
         setDayOfTheW(res.data.day_of_week)
-        console.log(res.data);
       })
       .catch((error) => {
         console.error('Error', error);
@@ -92,9 +91,15 @@ function App() {
         rotateIcon2={rotateIcon2}
         />
 
-        <div className='h-[35px] w-[90px] bg-white mt-[30px] ml-4 md:w-[156px] md:h-[56px] flex justify-end md:ml-[64px]  items-center pr-[2px] rounded-[20px] xl:ml-[116px] xl:relative xl:right-[10%]  dark:bg-black'>
-          <div className='h-[29px] w-[29px] bg-black rounded-[50%] md:w-[50px] md:h-[50px]  dark:mr-[55px] dark:bg-white absolute dark:md:mr-[100px]' style={{transition:'1s'}} onClick={darkTheme}>
-            <div onClick={toggleShavi}  className='text-white cursor-pointer flex justify-center pt-[2.5px] md:text-[30px]  dark:text-black'>{shavi}</div>
+        <div className='h-[35px] w-[90px] bg-white mt-[30px] ml-4 md:w-[156px] md:h-[56px]
+         flex justify-end md:ml-[64px]  items-center pr-[2px] rounded-[20px] xl:ml-[116px]
+          xl:relative xl:right-[10%]  dark:bg-black'>
+          <div className='
+          h-[29px] w-[29px] bg-black rounded-[50%] md:w-[50px] md:h-[50px] 
+           dark:mr-[55px] dark:bg-white absolute dark:md:mr-[100px]' style={{transition:'1s'}} onClick={darkTheme}>
+            <div onClick={toggleShavi}  
+            className='text-white cursor-pointer
+             flex justify-center pt-[2.5px] md:text-[30px]  dark:text-black'>{shavi}</div>
           </div>
         </div>
         <div>
