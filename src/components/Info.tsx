f4b5c2cc3122458230ebec8582eabf7e0eb5927d
bachidze <bachidze90@gmail.com>
2024-02-03 16:22:11 +0400

    Feat(remove/add):remove Dark/Txt Add/Img

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,28 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-interface InfoProps{
-    gaqroba:boolean;
-    setGaqroba:React.Dispatch<React.SetStateAction<boolean>>;
-    rotate:boolean;
-    rotateIcon:()=>void;
+interface InfoProps {
+  gaqroba: boolean;
+  setGaqroba: React.Dispatch<React.SetStateAction<boolean>>;
+  rotate: boolean;
+  rotateIcon: () => void;
 }
 
-const Info = ({gaqroba,setGaqroba,rotate,rotateIcon}:InfoProps) => {
+const Info = ({ gaqroba, setGaqroba, rotate, rotateIcon,  }: InfoProps) => {
   return (
     <div>
-        <div className='w-[100px] h-[35px] rounded-[20px] md:ml-[64px] md:w-[156px] md:h-[56px]  bg-[white] ml-4 flex justify-end items-center pr-1 mt-[90px] mb-[35px] xl:ml-[116px] xl:text-[25px] xl:relative xl:left-[70%] dark:bg-black'>
-          <h3 className='pr-[7px] text-[gray] text-[14px] md:text-[16px] md:font-bold md:pr-[18px] dark:text-white'>MORE</h3>
-          <div className='w-[30px] h-[30px] bg-black rounded-[50%] md:w-[50px] md:h-[50px] cursor-pointer flex justify-center items-center dark:bg-white'
-          onClick={()=>setGaqroba(!gaqroba)}>
-            <FontAwesomeIcon className={`transform ${rotate ? 'rotate-180' : 'rotate-0'} text-white transition duration-1000 dark:text-black`}
-             icon={faChevronDown} 
-             onClick={rotateIcon}
-             />
-          </div>
+      <div className='w-[100px] h-[35px] rounded-[20px] md:ml-[64px] md:w-[156px] md:h-[56px]  bg-[white] ml-4 flex justify-end items-center pr-1 mt-[90px] mb-[35px] xl:ml-[116px] xl:text-[25px] xl:relative xl:left-[70%] dark:bg-black'>
+        <h3 className=' pr-[7px] text-black text-[14px] md:text-[16px] md:font-bold md:pr-[18px] dark:text-white'>MORE</h3>
+        <div className='w-[30px] h-[30px] bg-black rounded-[50%] md:w-[50px] md:h-[50px] cursor-pointer flex justify-center items-center dark:bg-white'
+          onClick={() => { setGaqroba(!gaqroba)}}>
+          <FontAwesomeIcon className={`transform ${rotate ? 'rotate-180' : 'rotate-0'}  text-white  transition duration-1000 md:text-[30px] xl:text-[35px] dark:text-black`}
+            icon={faChevronDown}
+            onClick={rotateIcon}
+          />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -9,11 +9,11 @@ interface TimeProps{
 const Time = ({time,time2,time3}:TimeProps) => {
   return (
     <div className="md:pl-[58px] xl:ml-[36px] xl:relative xl:right-[10%] xl:top-[30px] ">
-         <div className='pl-4 pt-[220px] md:text-[22px] text-[white] flex gap-[5px] xl:text-[48px] dark:text-black'><img className="dark:hidden md:w-[28px] xl:w-[48px]" src="/assets/Combined Shape 1.svg" alt="mze" />
-         <img className="hidden text-[10px] md:w-[28px] xl:w-[48px] dark:text-black  dark:flex" src="/assets/Path (12).svg" alt="mtvare" />
+         <div className='pl-4 pt-[220px] md:text-[22px] text-[white] flex gap-[5px] xl:text-[48px] '><img className="dark:hidden md:w-[28px] xl:w-[48px]" src="/assets/Combined Shape 1.svg" alt="mze" />
+         <img className="hidden text-[10px] md:w-[28px] xl:w-[48px]  dark:flex" src="/assets/Path (12).svg" alt="mtvare" />
          {time}</div>
-        <div className='pl-4 text-xl pt-3 text-[white] md:text-[28px] xl:text-[48px] xl:pt-[30px] dark:text-black'>{time2}</div>
-        <div className='pl-4 pt-3 text-xl text-[white] md:text-[28px] xl:text-[48px] xl:pt-[35px] dark:text-black'>{time3}</div>
+        <div className='pl-4 text-xl pt-3 text-[white] md:text-[28px] xl:text-[48px] xl:pt-[30px]'>{time2}</div>
+        <div className='pl-4 pt-3 text-xl text-[white] md:text-[28px] xl:text-[48px] xl:pt-[35px]'>{time3}</div>
     </div>
   )
 }
